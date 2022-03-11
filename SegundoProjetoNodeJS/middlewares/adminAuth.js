88c4c0cbfb3c2df77dc9406795906efb41acb83f
Virgilio <virgilio.teixeira@outlook.com>
2022-03-11 05:16:30 -0400

    Add files via upload

---
+++ SegundoProjetoNodeJS/middlewares/adminAuth.js
@@ -0,0 +1,15 @@
+//middlewares sempre serebem três parâmetros (req, res e next)
+
+function adminAuth(req,res,next)
+{
+    if(req.session.user != undefined)
+    {
+        next()
+    }
+    else
+    {
+        res.redirect('/login')
+    }
+}
+
+module.exports = adminAuth
